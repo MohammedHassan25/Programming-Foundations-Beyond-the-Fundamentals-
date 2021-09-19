ff64133f10d0c55d282fb8047060e59e621295d8
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2021-09-19 02:46:42 +0200

    chap 05 : Planning a Program
    chap 05 : Planning a Program

---
+++ chap 05/01_Choosing a code style.py
@@ -0,0 +1,4 @@
+# ----------------------------------------------------------------------------------------------
+# Style Guides : Documentation on approaches to code as : Google Python
+# For Python developers, the most commonly used style guide is called (pep8 :https://pep8.org/)
+# ----------------------------------------------------------------------------------------------
