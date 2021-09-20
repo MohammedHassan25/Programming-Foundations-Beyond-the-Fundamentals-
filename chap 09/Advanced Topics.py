cb9db7b7e9e3a133798fef390d3656a166a07fe5
MohammedHassan25 <87442047+MohammedHassan25@users.noreply.github.com>
2021-09-20 06:00:52 +0200

    chap 09 : Advanced Topics
    chap 09 : Advanced Topics

---
+++ chap 09/Advanced Topics.py
@@ -0,0 +1,6 @@
+# ------------------------------------------------------------------------------------------------------------------------------
+# Memory management : Some languages allow programmers to write code that decides what's kept in memory and what's thrown away.
+# Each task is known as a thread.
+# Multithreading : The overall approach to writing code that executes threads concurrently
+# Algorithm : a set of instructions that describes how to get the exact result you want
+# ------------------------------------------------------------------------------------------------------------------------------
